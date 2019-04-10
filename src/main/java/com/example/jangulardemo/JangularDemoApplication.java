c2824ffd72843b9f701f1cda77c9ce8e07d4f27e
Mohamed Fofana <manoufof@hotmail.com>
2019-04-10 21:51:19 +0200

    Initial Commit   - Update project structure   - Angular 7 Build install Integration in Maven

---
+++ src/main/java/com/example/jangulardemo/JangularDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jangulardemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JangularDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JangularDemoApplication.class, args);
+	}
+
+}
